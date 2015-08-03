bbf8200c20899cc8cb01dfa98352cae01ea4dd1d
Ricardo Padilha <ricardospadilha@gmail.com>
2015-08-03 16:55:02 -0700

    Fixed bash path in exec

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,7 @@ if [ "${1:-}" = "build" ]; then
     git clone "${2}" "${_name}"
   fi
   cd "${_name}"
-  ./build.sh && cp *.tgz /dist/ || exec bin/bash
+  ./build.sh && cp *.tgz /dist/ || exec /bin/bash
   rm -f "/dist/.${2}"
 elif [ -z "${1:-}" ]; then
   exec /bin/bash
