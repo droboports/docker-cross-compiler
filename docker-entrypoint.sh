612bfb2c96e80a80708c2901d9cd4d85b83129cc
Ricardo Padilha <ricardospadilha@gmail.com>
2015-08-04 14:30:37 -0700

    Improved build handling when no package is generated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,12 @@ set -o errexit
 set -o nounset
 
 if [ "${1:-}" = "build" ]; then
+  shift
   set -o xtrace
   _name="$(basename "${2}" .git)"
+  # test write access to /dist
   touch "/dist/.${_name}"
+  rm -f "/dist/.${_name}"
   cd ~/build
   if [ "${_name}" = "${2}" ]; then
     git clone "https://github.com/droboports/${2}.git" "${_name}"
@@ -14,8 +17,12 @@ if [ "${1:-}" = "build" ]; then
     git clone "${2}" "${_name}"
   fi
   cd "${_name}"
-  ./build.sh && cp *.tgz /dist/ || exec /bin/bash
-  rm -f "/dist/.${2}"
+  ./build.sh "$@"
+  if [ -n "$(find . -maxdepth 1 -name '*.tgz' -print -quit)" ]; then
+    cp *.tgz /dist/
+  else
+    exec /bin/bash
+  fi
 elif [ -z "${1:-}" ]; then
   exec /bin/bash
 else
