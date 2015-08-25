7b610b9cc274ab0a45c46e05b694c13f804774f2
Ricardo Padilha <ricardospadilha@gmail.com>
2015-08-25 16:18:51 -0700

    Fixed drobo home path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ if [ "${1:-}" = "build" ]; then
   # test write access to /dist
   touch "/dist/.${_name}"
   rm -f "/dist/.${_name}"
-  cd ~/build
+  cd /home/drobo/build
   if [ "${_name}" = "${1}" ]; then
     git clone "https://github.com/droboports/${1}.git" "${_name}"
   else
